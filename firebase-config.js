96642c7e38d14a0def7e7b022a53ffd4266d83a2
drewharris20-eng <drewharris20@gmail.com>
2026-05-20 15:25:36 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "YOUR_SUPABASE_URL";
+const SUPABASE_URL = "https://kwkxbdgnrmisrmxeeywm.supabase.co";
 
-const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
+const SUPABASE_ANON_KEY = "sb_publishable_-A9SUiQui-o16qERCNiVpQ_ohoR1o10";
 
 const BUCKET_NAME = "wedding-galleries";
