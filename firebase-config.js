45d781fdb506cb8ab8bebbf6de4a8d3d428d8969
drewharris20-eng <drewharris20@gmail.com>
2026-05-20 22:28:20 -0500

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,5 +0,0 @@
-const SUPABASE_URL = "https://kwkxbdgnrmisrmxeeywm.supabase.co";
-
-const SUPABASE_ANON_KEY = "sb_publishable_-A9SUiQui-o16qERCNiVpQ_ohoR1o10";
-
-const BUCKET_NAME = "wedding-galleries";
